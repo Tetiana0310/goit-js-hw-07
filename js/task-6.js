ab712ddba44c9357833774975911ef967d2135cf
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-06-30 19:41:05 +0300

    Done task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxesBody = document.querySelector("#boxes")
+const numberInput = document.querySelector("input")
+const btnToCreate = document.querySelector("button[data-create]")
+const btnToDestroy = document.querySelector("button[data-destroy]")
+
+
+btnToCreate.addEventListener("click", () => {
+  const amount = numberInput.value;
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+
+  numberInput.value = "";
+})
+
+function createBoxes(amount) {
+  boxesBody.innerHTML = "";
+
+  const boxes = [];
+   for (let i = 1; i <= amount; i += 1) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    boxes.push(box);
+  }
+ boxesBody.append(...boxes)
+}
+
+function destroyBoxes() {
+  boxesBody.innerHTML = "";
+}
+
+btnToDestroy.addEventListener("click", destroyBoxes);
