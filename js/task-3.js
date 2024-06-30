e76195491b576ccbecb20e9b18d2810cbab35cac
Tetiana0310 <tetiana.posmashna@gmail.com>
2024-06-30 18:21:09 +0300

    Done task - 4

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@ const input = document.querySelector("#name-input");
 const name = document.querySelector("#name-output");
 
 function onInputChange(event) {
-    if (event.currentTarget.value === "" || event.currentTarget.value.trim().length === 0) {
+    if (event.currentTarget.value === "" || event.currentTarget.value.trim().length === 0)
+    {
         name.textContent = "Anonymous";
     } else {
         name.textContent = event.currentTarget.value.trim();
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    // console.log(event)
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        alert('All form fields must be filled in');
+    } else {
+        console.log({ Email: email.value.trim(), Password: password.value.trim() });
+        event.currentTarget.reset();
+    }
+}
